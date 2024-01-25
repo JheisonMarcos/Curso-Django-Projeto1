d7cf6d7c455653efae757e439cc08c6deac94002
Jheison Francelino <jheisonmarcos1@gmail.com>
2024-01-25 17:15:53 -0300

    add icon test

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', contex={
-        'name': 'Jheison Francelino',
+    return render(request, 'recipes/home.html', context={
+        'name': 'Jheison',
     })
