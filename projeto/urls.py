ad91a9e13c032f59c5f08c7ec24d7b64b65f35f5
Jheison Francelino <jheisonmarcos1@gmail.com>
2024-01-24 21:56:44 -0300

    transições de urls.py em "recipes"

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,32 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 
-# HTTP REQUEST <- HTTP RESPONSE
-
-# HTTP REQUEST
-
-
-def home(request): 
-    return HttpResponse('HOME')
-
-
-
-def sobre(request): 
-    return HttpResponse('sobre')
-
-
-
-def contato(request): 
-    return HttpResponse('contato')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), # HOME
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato),  # /contato/
+    path('', include('recipes.urls')), # / 
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def home(request): 
+    return HttpResponse('HOME 1')
+
+
+def sobre(request): 
+    return HttpResponse('sobre 1')
+
+
+def contato(request): 
+    return HttpResponse('contato 1')
