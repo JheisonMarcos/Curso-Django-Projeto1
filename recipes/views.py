0d5c729f0e9359599a16f467b77e1044eb825f9a
Jheison Francelino <jheisonmarcos1@gmail.com>
2024-01-25 16:56:52 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
-# Create your views here.
 
 def home(request):
     return render(request, 'recipes/home.html', contex={
         'name': 'Jheison Francelino',
     })
-
-
-def sobre(request):
-    return HttpResponse('sobre 1')
-
-
-def contato(request):
-    return render(request, 'temp.html')
